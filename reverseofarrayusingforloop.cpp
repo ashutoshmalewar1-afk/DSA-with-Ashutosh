06bd27f4534effee455f42dd59fe6b436259d36c
Ashutosh Malewar <ashutoshmalewar1@gmail.com>
2026-01-21 21:32:23 +0530

    Add files via upload

---
+++ reverseofarrayusingforloop.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void display(vector<int> v){
+   for(int i=0;i<v.size();i++){
+    cout<<v[i]<<" ";
+   }
+   cout<<endl;
+}
+int main(){
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(5);
+    v.push_back(6);
+    display(v);
+
+
+// int i=0;
+// int j=v.size()-1;
+// while(i<=j){
+//     int temp=v[i];
+//         v[i]=v[j];
+//         v[j]=temp;
+//         i++;
+//         j--;
+// }
+    for(int i=0,j=v.size()-1;i<=j;i++,j--){
+        int temp=v[i];
+        v[i]=v[j];
+        v[j]=temp;
+    }
+display(v);
+}
