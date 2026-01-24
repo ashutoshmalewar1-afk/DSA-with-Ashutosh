45433140f5219fe37cb593bd322405d4f0d72786
Ashutosh Malewar <ashutoshmalewar1@gmail.com>
2026-01-24 22:43:06 +0530

    Add files via upload

---
+++ pp2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,n,m;
+    cout<<"enter the no of rows"<<endl;
+    cin>>m;
+    
+    for(i=1;i<=m;i++){ //rows
+        for(j=1;j<=m;j++){   //col
+            
+        cout<<" * ";
+    }
+        cout<<endl;
+    }
+    return 0;
+}
