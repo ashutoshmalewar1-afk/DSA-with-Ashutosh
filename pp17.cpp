8dd58ed1c19274530049e41995efff56264335d3
Ashutosh Malewar <ashutoshmalewar1@gmail.com>
2026-01-27 20:00:55 +0530

    Add files via upload

---
+++ pp17.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,n;
+    cout<<"enter the side:"<<endl;
+    cin>>n;
+    
+   for(i=1;i<=n;i++)
+   {
+   
+    for(j=1;j<=n+1-i;j++)           //for ulta star the condition is j<=n+1-i
+    {
+        cout<<j;
+        
+    }
+    cout<<endl;
+   }
+    return 0;
+}
