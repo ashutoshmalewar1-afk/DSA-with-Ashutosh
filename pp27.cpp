93115b7db335ae476e3a590662ef5b8e89f6b71e
Ashutosh Malewar <ashutoshmalewar1@gmail.com>
2026-02-01 08:45:07 +0530

    Add files via upload

---
+++ pp27.cpp
@@ -0,0 +1,24 @@
+    #include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,n;
+    cout<<"enter the side:"<<endl;
+    cin>>n;
+    
+   for(i=1;i<=n;i++){
+    for(j=1;j<=i;j++){
+      if(j%2!=0)
+      {
+        cout<<"1"<<" ";
+      }
+      else
+      {
+        cout<<"0"<<" ";
+      }
+        }
+       cout<<endl;
+    }
+    return 0;
+   }
+ 
