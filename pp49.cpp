369f0f6be331bf0bfc70f34664d35e7fa43ad783
Ashutosh Malewar <ashutoshmalewar1@gmail.com>
2026-02-10 17:51:56 +0530

    Add files via upload

---
+++ pp49.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,n,j,k;
+    cout<<"enter the side."<<endl;
+    cin>>n;
+    int star=n;
+    int space=1;
+    
+    for(i=1;i<=2*n+1;i++){            //first line
+        cout<<char(i+64);
+    }
+    cout<<endl;
+    for(i=1;i<=n;i++){                
+      int a=1;
+      for(k=1;k<=star;k++){            //star
+        cout<<char(a+64);
+        a++;
+       }
+       
+       for(j=1;j<=space;j++){         //space
+        cout<<" ";
+        a++;
+       }
+      
+       for(k=1;k<=star;k++){           //star
+        cout<<char(a+64);
+        a++;
+       }
+         star--;
+        space+=2;
+        cout<<endl;
+       
+        
+        }
+       return 0;
+        }
+         
+    
