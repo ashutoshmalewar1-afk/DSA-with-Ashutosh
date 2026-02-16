f96e723692c9da80ea8df85f609e6f4ed38a74b3
Ashutosh Malewar <ashutoshmalewar1@gmail.com>
2026-02-16 08:25:03 +0530

    Add files via upload

---
+++ fp1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void usa(){          //function declare
+    cout<<"hello my name is ashutosh.";
+    return;
+}
+void india(){
+    cout<<"you are in india.";
+    retrun;
+}
+int main(){
+    usa();    //function call
+    india();
+}
